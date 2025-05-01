3321481858a1ae3ff8c033ef778c8d96cfd915c0
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2025-05-01 18:02:01 +0900

    Use crane for rust build

--- flake.nix
+++ flake.nix
@@ -5,17 +5,22 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     fenix.url = "github:nix-community/fenix";
+    crane.url = "github:ipetkov/crane";
   };
 
-  outputs = { self, nixpkgs, flake-utils, fenix }:
+  outputs = { self, nixpkgs, flake-utils, fenix, crane }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ fenix.overlays.default ];
         };
+        craneLib =
+          (crane.mkLib pkgs).overrideToolchain (p: p.fenix.stable.toolchain);
+        crate = pkgs.callPackage ./nix/cargo.nix { inherit craneLib; };
       in {
-        devShells.default = pkgs.callPackage ./nix/dev-shell.nix { };
-        packages.default = pkgs.callPackage ./nix/toy-cpu-pathtracing.nix { };
+        devShells.default = craneLib.devShell { };
+        packages.default = crate.build;
+        checks = crate.checks;
       });
 }
