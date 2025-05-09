c10a3fdecf69d7cd4194f0c6376035c0baa77eee
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2025-05-10 03:00:04 +0900

    Change the generation of tables for RgbSigmoidPolynomial to a build script.

--- color/src/gamut.rs
+++ color/src/gamut.rs
@@ -1,14 +1,18 @@
 //! 色域を表すトレイトと、いくつかの色域の実装。
 
-use math::{cube_root, mat3_inverse, mat3_mul_mat3, mat3_mul_vec3};
-
 /// 色域を表すトレイト。
 pub trait ColorGamut: Sync + Send + Clone {
-    /// 色域をXYZからRGBに変換する。
+    /// 色域の構造体を作成する。
+    fn new() -> Self;
+
+    /// 色をXYZからこの色域のRGBに変換する。
     fn xyz_to_rgb(&self, xyz: glam::Vec3) -> glam::Vec3;
 
-    /// 色域をRGBからXYZに変換する。
+    /// この色域の色をRGBからXYZに変換する。
     fn rgb_to_xyz(&self, rgb: glam::Vec3) -> glam::Vec3;
+
+    /// この色域の色をRGBからLabに変換する。
+    fn rgb_to_lab(&self, rgb: glam::Vec3) -> glam::Vec3;
 }
 
 const fn xyy_to_xyz(xy: glam::Vec2, y: f32) -> glam::Vec3 {
@@ -24,13 +28,8 @@ const fn xy_to_xyz(xy: glam::Vec2) -> glam::Vec3 {
     xyy_to_xyz(xy, 1.0)
 }
 
-/// RGBからXYZに変換する行列を計算する。
-const fn rgb_to_xyz(
-    r_xy: glam::Vec2,
-    g_xy: glam::Vec2,
-    b_xy: glam::Vec2,
-    w: glam::Vec2,
-) -> glam::Mat3 {
+/// RGBのxy色度とホワイトポイントのxy色度からXYZに変換する行列を計算する。
+fn rgb_to_xyz(r_xy: glam::Vec2, g_xy: glam::Vec2, b_xy: glam::Vec2, w: glam::Vec2) -> glam::Mat3 {
     let r = xy_to_xyz(r_xy);
     let g = xy_to_xyz(g_xy);
     let b = xy_to_xyz(b_xy);
@@ -38,16 +37,15 @@ const fn rgb_to_xyz(
 
     let rgb = glam::Mat3::from_cols(r, g, b);
 
-    let c = mat3_mul_vec3(mat3_inverse(rgb), w);
-
-    mat3_mul_mat3(rgb, glam::Mat3::from_diagonal(c))
+    let c = rgb.inverse() * w;
+    rgb * glam::Mat3::from_diagonal(c)
 }
 
 /// XYZからLabに変換する関数。
-pub const fn xyz_to_lab(xyz: glam::Vec3) -> glam::Vec3 {
-    const fn f(t: f32) -> f32 {
+pub fn xyz_to_lab(xyz: glam::Vec3) -> glam::Vec3 {
+    fn f(t: f32) -> f32 {
         if t > 0.008856 {
-            cube_root(t)
+            t.powf(1.0 / 3.0)
         } else {
             (t * 903.3 + 16.0) / 116.0
         }
@@ -67,16 +65,16 @@ pub const fn xyz_to_lab(xyz: glam::Vec3) -> glam::Vec3 {
 }
 
 /// RGBからCIE Labに変換する関数。
-const fn lab(rgb: glam::Vec3, rgb_to_xyz: glam::Mat3) -> glam::Vec3 {
-    const fn f(t: f32) -> f32 {
+fn lab(rgb: glam::Vec3, rgb_to_xyz: glam::Mat3) -> glam::Vec3 {
+    fn f(t: f32) -> f32 {
         if t > 0.008856 {
-            cube_root(t)
+            t.powf(1.0 / 3.0)
         } else {
             (903.3 * t + 16.0) / 116.0
         }
     }
     // RGBをXYZに変換する。
-    let xyz = mat3_mul_vec3(rgb_to_xyz, rgb);
+    let xyz = rgb_to_xyz * rgb;
 
     // D50白色点のXYZ値を取得する。
     let w = glam::vec2(0.34567, 0.35850);
@@ -101,15 +99,15 @@ pub struct SrgbGamut {
     xyz_to_rgb: glam::Mat3,
     rgb_to_xyz: glam::Mat3,
 }
-impl SrgbGamut {
+impl ColorGamut for SrgbGamut {
     /// sRGBの色域を生成する。
-    pub const fn new() -> Self {
+    fn new() -> Self {
         let r_xy = glam::vec2(0.6400, 0.3300);
         let g_xy = glam::vec2(0.3000, 0.6000);
         let b_xy = glam::vec2(0.1500, 0.0600);
         let w_xy = glam::vec2(0.3127, 0.3290);
         let rgb_to_xyz = rgb_to_xyz(r_xy, g_xy, b_xy, w_xy);
-        let xyz_to_rgb = mat3_inverse(rgb_to_xyz);
+        let xyz_to_rgb = rgb_to_xyz.inverse();
         Self {
             xyz_to_rgb,
             rgb_to_xyz,
@@ -117,11 +115,10 @@ impl SrgbGamut {
     }
 
     /// sRGBの色をCIE Labに変換する。
-    pub const fn rgb_to_lab(&self, rgb: glam::Vec3) -> glam::Vec3 {
+    fn rgb_to_lab(&self, rgb: glam::Vec3) -> glam::Vec3 {
         lab(rgb, self.rgb_to_xyz)
     }
-}
-impl ColorGamut for SrgbGamut {
+
     fn xyz_to_rgb(&self, xyz: glam::Vec3) -> glam::Vec3 {
         self.xyz_to_rgb * xyz
     }
@@ -137,15 +134,15 @@ pub struct DciP3D65Gamut {
     xyz_to_rgb: glam::Mat3,
     rgb_to_xyz: glam::Mat3,
 }
-impl DciP3D65Gamut {
+impl ColorGamut for DciP3D65Gamut {
     /// DCI-P3 D65の色域を生成する。
-    pub const fn new() -> Self {
+    fn new() -> Self {
         let r_xy = glam::vec2(0.6800, 0.3200);
         let g_xy = glam::vec2(0.2650, 0.6900);
         let b_xy = glam::vec2(0.1500, 0.0600);
         let w_xy = glam::vec2(0.3127, 0.3290);
         let rgb_to_xyz = rgb_to_xyz(r_xy, g_xy, b_xy, w_xy);
-        let xyz_to_rgb = mat3_inverse(rgb_to_xyz);
+        let xyz_to_rgb = rgb_to_xyz.inverse();
         Self {
             xyz_to_rgb,
             rgb_to_xyz,
@@ -153,11 +150,10 @@ impl DciP3D65Gamut {
     }
 
     /// DCI-P3 D65の色をCIE Labに変換する。
-    pub const fn rgb_to_lab(&self, rgb: glam::Vec3) -> glam::Vec3 {
+    fn rgb_to_lab(&self, rgb: glam::Vec3) -> glam::Vec3 {
         lab(rgb, self.rgb_to_xyz)
     }
-}
-impl ColorGamut for DciP3D65Gamut {
+
     fn xyz_to_rgb(&self, xyz: glam::Vec3) -> glam::Vec3 {
         self.xyz_to_rgb * xyz
     }
@@ -173,15 +169,15 @@ pub struct AdobeRgbGamut {
     xyz_to_rgb: glam::Mat3,
     rgb_to_xyz: glam::Mat3,
 }
-impl AdobeRgbGamut {
+impl ColorGamut for AdobeRgbGamut {
     /// Adobe RGBの色域を生成する。
-    pub const fn new() -> Self {
+    fn new() -> Self {
         let r_xy = glam::vec2(0.6400, 0.3300);
         let g_xy = glam::vec2(0.2100, 0.7100);
         let b_xy = glam::vec2(0.1500, 0.0600);
         let w_xy = glam::vec2(0.3127, 0.3290);
         let rgb_to_xyz = rgb_to_xyz(r_xy, g_xy, b_xy, w_xy);
-        let xyz_to_rgb = mat3_inverse(rgb_to_xyz);
+        let xyz_to_rgb = rgb_to_xyz.inverse();
         Self {
             xyz_to_rgb,
             rgb_to_xyz,
@@ -189,11 +185,10 @@ impl AdobeRgbGamut {
     }
 
     /// Adobe RGBの色をCIE Labに変換する。
-    pub const fn rgb_to_lab(&self, rgb: glam::Vec3) -> glam::Vec3 {
+    fn rgb_to_lab(&self, rgb: glam::Vec3) -> glam::Vec3 {
         lab(rgb, self.rgb_to_xyz)
     }
-}
-impl ColorGamut for AdobeRgbGamut {
+
     fn xyz_to_rgb(&self, xyz: glam::Vec3) -> glam::Vec3 {
         self.xyz_to_rgb * xyz
     }
@@ -209,15 +204,15 @@ pub struct Rec2020Gamut {
     xyz_to_rgb: glam::Mat3,
     rgb_to_xyz: glam::Mat3,
 }
-impl Rec2020Gamut {
+impl ColorGamut for Rec2020Gamut {
     /// Rec. 2020の色域を生成する。
-    pub const fn new() -> Self {
+    fn new() -> Self {
         let r_xy = glam::vec2(0.7080, 0.2920);
         let g_xy = glam::vec2(0.1700, 0.7970);
         let b_xy = glam::vec2(0.1310, 0.0460);
         let w_xy = glam::vec2(0.3127, 0.3290);
         let rgb_to_xyz = rgb_to_xyz(r_xy, g_xy, b_xy, w_xy);
-        let xyz_to_rgb = mat3_inverse(rgb_to_xyz);
+        let xyz_to_rgb = rgb_to_xyz.inverse();
         Self {
             xyz_to_rgb,
             rgb_to_xyz,
@@ -225,11 +220,10 @@ impl Rec2020Gamut {
     }
 
     /// Rec. 2020の色をCIE Labに変換する。
-    pub const fn rgb_to_lab(&self, rgb: glam::Vec3) -> glam::Vec3 {
+    fn rgb_to_lab(&self, rgb: glam::Vec3) -> glam::Vec3 {
         lab(rgb, self.rgb_to_xyz)
     }
-}
-impl ColorGamut for Rec2020Gamut {
+
     fn xyz_to_rgb(&self, xyz: glam::Vec3) -> glam::Vec3 {
         self.xyz_to_rgb * xyz
     }
@@ -246,15 +240,15 @@ pub struct AcesCgGamut {
     xyz_to_rgb: glam::Mat3,
     rgb_to_xyz: glam::Mat3,
 }
-impl AcesCgGamut {
+impl ColorGamut for AcesCgGamut {
     /// ACEScgの色域を生成する。
-    pub const fn new() -> Self {
+    fn new() -> Self {
         let r_xy = glam::vec2(0.7130, 0.2930);
         let g_xy = glam::vec2(0.1650, 0.8300);
         let b_xy = glam::vec2(0.1280, 0.0440);
         let w_xy = glam::vec2(0.32168, 0.33767);
         let rgb_to_xyz = rgb_to_xyz(r_xy, g_xy, b_xy, w_xy);
-        let xyz_to_rgb = mat3_inverse(rgb_to_xyz);
+        let xyz_to_rgb = rgb_to_xyz.inverse();
         Self {
             xyz_to_rgb,
             rgb_to_xyz,
@@ -262,11 +256,10 @@ impl AcesCgGamut {
     }
 
     /// ACEScgの色をCIE Labに変換する。
-    pub const fn rgb_to_lab(&self, rgb: glam::Vec3) -> glam::Vec3 {
+    fn rgb_to_lab(&self, rgb: glam::Vec3) -> glam::Vec3 {
         lab(rgb, self.rgb_to_xyz)
     }
-}
-impl ColorGamut for AcesCgGamut {
+
     fn xyz_to_rgb(&self, xyz: glam::Vec3) -> glam::Vec3 {
         self.xyz_to_rgb * xyz
     }
@@ -283,15 +276,15 @@ pub struct Aces2065_1Gamut {
     xyz_to_rgb: glam::Mat3,
     rgb_to_xyz: glam::Mat3,
 }
-impl Aces2065_1Gamut {
+impl ColorGamut for Aces2065_1Gamut {
     /// ACES2065-1の色域を生成する。
-    pub const fn new() -> Self {
+    fn new() -> Self {
         let r_xy = glam::vec2(0.73470, 0.26530);
         let g_xy = glam::vec2(0.00000, 1.00000);
         let b_xy = glam::vec2(0.00010, -0.07700);
         let w_xy = glam::vec2(0.32168, 0.33767);
         let rgb_to_xyz = rgb_to_xyz(r_xy, g_xy, b_xy, w_xy);
-        let xyz_to_rgb = mat3_inverse(rgb_to_xyz);
+        let xyz_to_rgb = rgb_to_xyz.inverse();
         Self {
             xyz_to_rgb,
             rgb_to_xyz,
@@ -299,11 +292,10 @@ impl Aces2065_1Gamut {
     }
 
     /// ACES2065-1の色をCIE Labに変換する。
-    pub const fn rgb_to_lab(&self, rgb: glam::Vec3) -> glam::Vec3 {
+    fn rgb_to_lab(&self, rgb: glam::Vec3) -> glam::Vec3 {
         lab(rgb, self.rgb_to_xyz)
     }
-}
-impl ColorGamut for Aces2065_1Gamut {
+
     fn xyz_to_rgb(&self, xyz: glam::Vec3) -> glam::Vec3 {
         self.xyz_to_rgb * xyz
     }
--- math/src/matrix.rs
+++ math/src/matrix.rs
@@ -1,83 +1,22 @@
 //! 行列の計算を定義するモジュール。
 //! glamで定義されていない追加の計算を提供する。
 
-/// glam::Mat3とglam::Mat3の積をコンパイル時に計算する関数。
-pub const fn mat3_mul_mat3(m1: glam::Mat3, m2: glam::Mat3) -> glam::Mat3 {
-    glam::Mat3::from_cols(
-        glam::vec3(
-            m1.x_axis.x * m2.x_axis.x + m1.y_axis.x * m2.x_axis.y + m1.z_axis.x * m2.x_axis.z,
-            m1.x_axis.y * m2.x_axis.x + m1.y_axis.y * m2.x_axis.y + m1.z_axis.y * m2.x_axis.z,
-            m1.x_axis.z * m2.x_axis.x + m1.y_axis.z * m2.x_axis.y + m1.z_axis.z * m2.x_axis.z,
-        ),
-        glam::vec3(
-            m1.x_axis.x * m2.y_axis.x + m1.y_axis.x * m2.y_axis.y + m1.z_axis.x * m2.y_axis.z,
-            m1.x_axis.y * m2.y_axis.x + m1.y_axis.y * m2.y_axis.y + m1.z_axis.y * m2.y_axis.z,
-            m1.x_axis.z * m2.y_axis.x + m1.y_axis.z * m2.y_axis.y + m1.z_axis.z * m2.y_axis.z,
-        ),
-        glam::vec3(
-            m1.x_axis.x * m2.z_axis.x + m1.y_axis.x * m2.z_axis.y + m1.z_axis.x * m2.z_axis.z,
-            m1.x_axis.y * m2.z_axis.x + m1.y_axis.y * m2.z_axis.y + m1.z_axis.y * m2.z_axis.z,
-            m1.x_axis.z * m2.z_axis.x + m1.y_axis.z * m2.z_axis.y + m1.z_axis.z * m2.z_axis.z,
-        ),
-    )
-}
-
-/// glam::Mat3とglam::Vec3の積をコンパイル時に計算する関数。
-pub const fn mat3_mul_vec3(m: glam::Mat3, v: glam::Vec3) -> glam::Vec3 {
-    glam::vec3(
-        m.x_axis.x * v.x + m.y_axis.x * v.y + m.z_axis.x * v.z,
-        m.x_axis.y * v.x + m.y_axis.y * v.y + m.z_axis.y * v.z,
-        m.x_axis.z * v.x + m.y_axis.z * v.y + m.z_axis.z * v.z,
-    )
-}
-
-/// glam::Mat3の逆行列をコンパイル時に計算する関数。
-pub const fn mat3_inverse(m: glam::Mat3) -> glam::Mat3 {
-    let det = m.x_axis.x * (m.y_axis.y * m.z_axis.z - m.z_axis.y * m.y_axis.z)
-        - m.y_axis.x * (m.x_axis.y * m.z_axis.z - m.z_axis.y * m.x_axis.z)
-        + m.z_axis.x * (m.x_axis.y * m.y_axis.z - m.y_axis.y * m.x_axis.z);
-    let inv_det = 1.0 / det;
-
-    glam::Mat3::from_cols(
-        glam::vec3(
-            (m.y_axis.y * m.z_axis.z - m.z_axis.y * m.y_axis.z) * inv_det,
-            -(m.x_axis.y * m.z_axis.z - m.z_axis.y * m.x_axis.z) * inv_det,
-            (m.x_axis.y * m.y_axis.z - m.y_axis.y * m.x_axis.z) * inv_det,
-        ),
-        glam::vec3(
-            -(m.y_axis.x * m.z_axis.z - m.z_axis.x * m.y_axis.z) * inv_det,
-            (m.x_axis.x * m.z_axis.z - m.z_axis.x * m.x_axis.z) * inv_det,
-            -(m.x_axis.x * m.y_axis.z - m.y_axis.x * m.x_axis.z) * inv_det,
-        ),
-        glam::vec3(
-            (m.y_axis.x * m.z_axis.y - m.z_axis.x * m.y_axis.y) * inv_det,
-            -(m.x_axis.x * m.z_axis.y - m.z_axis.x * m.x_axis.y) * inv_det,
-            (m.x_axis.x * m.y_axis.y - m.y_axis.x * m.x_axis.y) * inv_det,
-        ),
-    )
-}
-
-/// glam::Mat3のLUP分解（LU分解+ピボット行列）をコンパイル時に計算する関数。
-pub const fn lup_decompose(m: glam::Mat3, epsilon: f32) -> (glam::Mat3, glam::Mat3, [usize; 3]) {
+/// glam::Mat3のLUP分解（LU分解+ピボット行列）を計算する関数。
+pub fn lup_decompose(m: glam::Mat3, epsilon: f32) -> (glam::Mat3, glam::Mat3, [usize; 3]) {
     let mut a = m.to_cols_array(); // [f32; 9]、列優先（column-major）
     let mut p = [0, 1, 2];
-    let mut l = [0.0; 9];
-    let mut u = [0.0; 9];
 
-    let mut i = 0;
-    while i < 3 {
+    for i in 0..3 {
         // ピボット選択
         let mut max_row = i;
         let mut max_val = a[i + 0 * 3].abs();
 
-        let mut k = i + 1;
-        while k < 3 {
+        for k in 0..3 {
             let v = a[i + k * 3].abs();
             if v > max_val {
                 max_val = v;
                 max_row = k;
             }
-            k += 1;
         }
 
         // 特異行列の検出
@@ -91,43 +30,39 @@ pub const fn lup_decompose(m: glam::Mat3, epsilon: f32) -> (glam::Mat3, glam::Ma
             p[i] = p[max_row];
             p[max_row] = tmp;
 
-            let mut j = 0;
-            while j < 3 {
+            for j in 0..3 {
                 let idx1 = j + i * 3;
                 let idx2 = j + max_row * 3;
                 let temp = a[idx1];
                 a[idx1] = a[idx2];
                 a[idx2] = temp;
-                j += 1;
             }
         }
 
-        // LU 分解処理
-        let mut j = 0;
-        while j < 3 {
-            if j < i {
-                l[j + i * 3] = 0.0;
-            } else if j == i {
-                l[j + i * 3] = 1.0;
-            } else {
-                l[j + i * 3] = a[i + j * 3] / a[i + i * 3];
-            }
-            u[i + j * 3] = if j < i { 0.0 } else { a[i + j * 3] };
-            j += 1;
-        }
-
-        let mut j = i + 1;
-        while j < 3 {
+        // LU更新（U上三角とL下三角をa内に同時格納）
+        for j in (i + 1)..3 {
             let factor = a[i + j * 3] / a[i + i * 3];
-            let mut k = i;
-            while k < 3 {
-                a[k + j * 3] -= factor * a[k + i * 3];
-                k += 1;
+            for k in i..3 {
+                a[k + j * 3] -= factor * a[k + i * 3]; // Uの該当要素（上三角）として格納
             }
-            j += 1;
+            a[i + j * 3] = factor; // Lの該当要素（下三角）として格納
         }
+    }
 
-        i += 1;
+    // LとUを構築
+    let mut l = [0.0; 9];
+    let mut u = [0.0; 9];
+    for col in 0..3 {
+        for row in 0..3 {
+            if row > col {
+                l[col * 3 + row] = a[col * 3 + row]; // Lの下三角
+            } else if row == col {
+                l[col * 3 + row] = 1.0; // Lの対角
+                u[col * 3 + row] = a[col * 3 + row]; // Uの対角
+            } else {
+                u[col * 3 + row] = a[col * 3 + row]; // Uの上三角
+            }
+        }
     }
 
     (
@@ -138,7 +73,7 @@ pub const fn lup_decompose(m: glam::Mat3, epsilon: f32) -> (glam::Mat3, glam::Ma
 }
 
 /// LUP分解の結果を使って連立方程式Ax = bの解xを解く関数。
-pub const fn lup_solve(l: glam::Mat3, u: glam::Mat3, p: [usize; 3], b: glam::Vec3) -> glam::Vec3 {
+pub fn lup_solve(l: glam::Mat3, u: glam::Mat3, p: [usize; 3], b: glam::Vec3) -> glam::Vec3 {
     let l = l.to_cols_array();
     let u = u.to_cols_array();
     let b = b.to_array();
