237eaad78e53f76cad33937723067464cd319ec5
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2025-06-16 17:25:12 +0900

    Remove unnecessary texture sampler trait

--- scene/src/texture/normal_texture.rs
+++ scene/src/texture/normal_texture.rs
@@ -3,7 +3,7 @@
 use super::{
     config::TextureConfig,
     loader::{ImageData, load_rgb_image},
-    sampler::{TextureSample, bilinear_sample_rgb, bilinear_sample_rgb_f32},
+    sampler::{bilinear_sample_rgb, bilinear_sample_rgb_f32},
 };
 use glam::Vec2;
 use math::{Normal, ShadingTangent};
@@ -21,6 +21,17 @@ impl NormalTexture {
     pub fn load(config: TextureConfig, flip_y: bool) -> Result<Arc<Self>, image::ImageError> {
         let data = load_rgb_image(&config.path)?;
         Ok(Arc::new(Self { data, flip_y }))
+    }
+
+    /// UV座標でテクスチャをサンプリングしてRGB値を返す。
+    pub fn sample(&self, uv: Vec2) -> [f32; 3] {
+        match &self.data {
+            ImageData::Rgb8(data, width, height) => bilinear_sample_rgb(data, *width, *height, uv),
+            ImageData::RgbF32(data, width, height) => {
+                bilinear_sample_rgb_f32(data, *width, *height, uv)
+            }
+            _ => [0.5, 0.5, 1.0], // 不正なデータタイプの場合はデフォルトノーマル
+        }
     }
 
     /// UV座標でノーマルマップをサンプリングし、接空間ノーマルを取得する。
@@ -49,18 +60,6 @@ impl NormalTexture {
         } else {
             // 無効な場合はZ軸方向（デフォルトノーマル）
             Normal::new(0.0, 0.0, 1.0)
-        }
-    }
-}
-
-impl TextureSample<[f32; 3]> for NormalTexture {
-    fn sample(&self, uv: Vec2) -> [f32; 3] {
-        match &self.data {
-            ImageData::Rgb8(data, width, height) => bilinear_sample_rgb(data, *width, *height, uv),
-            ImageData::RgbF32(data, width, height) => {
-                bilinear_sample_rgb_f32(data, *width, *height, uv)
-            }
-            _ => [0.5, 0.5, 1.0], // 不正なデータタイプの場合はデフォルトノーマル
         }
     }
 }
